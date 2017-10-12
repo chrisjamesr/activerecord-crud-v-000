cc50d7115900a2d3b0e118f35e819eb93e913fa4
Chris Rudolph <chrisjames.r@gmail.com>
2017-10-12 20:53:02 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,25 +6,19 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new
   movie.title = "This is a title."
-  __
+  movie.save
 end
 
-def can_be_created_with_a_hash_of_attributes
-  attributes = {
-      title: "The Sting",
-      release_date: 1973,
-      director: "George Roy Hill",
-      lead: "Paul Newman",
-      in_theaters: false
-  }
-  movie = __
+def can_be_created_with_a_hash_of_attributes(attributes)
+  movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block
-  Movie.create do |m|
-    __
+def can_be_created_in_a_block(title, year)
+  movie = Movie.create do |m|
+    m.title = title
+    m.release_date = year
   end
 end
 
@@ -52,7 +46,7 @@ def can_find_by_multiple_attributes
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by 
+  # For this test return all movies released after 2002 and ordered by
   # release date descending
   __
 end
@@ -92,11 +86,3 @@ def can_destroy_all_items_at_once
   end
   __
 end
-
-
-
-
-
-
-
-
